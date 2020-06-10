8f83626bc81bf2439d97cc15c1c4088fe4ec577c
victoria <brainwashria86@gmail.com>
2020-06-10 22:23:41 +0400

    localstorage fixed onmouseover/onmouseleave buttons added

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 'use strict'
-const json = {
-    records: [{
-        text: 'record 1',
-    }, {
-        text: 'record 2',
-    }]
+let json = {
+    records: []
 };
 
+//функция добавления задач
 function createNewTask() {
     let taskValue = textarea.value;
     if(!taskValue) return;
-    let newLi = document.createElement('li');
-    newLi.innerText = taskValue;
-    newLi.classList.add('taskList');
-    todoUl.append(newLi);
-    json.records.push({text: `${taskValue}`} )
-    console.log(json);
-    textarea.value = ''
+    todoUl.insertAdjacentHTML('beforeend', `<li class="taskList">${taskValue}<div id="tasksButtonsDiv"><button class="editButton tasksButtons"></button><button class="deleteButton tasksButtons"></button></div></li>`)
+    json.records.push({text: `${taskValue}`, timestamp: Date.now()});
+    textarea.value = '';
     localStorage.setItem('user', JSON.stringify(json));
 }
+
+
+
+
+
 
 let textarea = document.querySelector('textarea');
 let todoUl = document.getElementById('todoUl')
 let addButton = document.getElementById('AddCard');
 
+todoUl.onmouseover = function(event) {
+    let target = event.target;
+    if(target.tagName !== 'LI') return
+    let buttonsDiv = document.getElementById('tasksButtonsDiv');
+    buttonsDiv.style.display = 'block';
+}
+
+todoUl.onmouseout = function(event) {
+    let target = event.target;
+    if(target.tagName !== 'LI') return
+    if(event.relatedTarget.tagName === 'BUTTON' && target.tagName === 'LI') return;
+    let buttonsDiv = document.getElementById('tasksButtonsDiv');
+    buttonsDiv.style.display = 'none';
+
+}
 
 // 1. Запрос к серверу
 // 2. ответ от сервера записываем в виде json'a в localstorage
 // 3. берем из localstorage json и парсим в список
 
-
 addButton.onclick = createNewTask;
 
-
 window.onload = function() {
-    console.log(localStorage.getItem('user'));
     let storageFile = JSON.parse(localStorage.getItem('user'));
-    console.log(storageFile);
+    if(!storageFile) return
+    json = storageFile
     for(let record of storageFile.records) {
-        console.log(record.text);
-        let li = document.createElement('li');
-        li.innerText = record.text;
-        li.classList.add('taskList');
-        todoUl.append(li);
+        todoUl.insertAdjacentHTML('beforeend', `<li class="taskList">${record.text}<div id="tasksButtonsDiv"><button class="editButton tasksButtons"></button><button class="deleteButton tasksButtons"></button></div></li>`)
+
     }
 }
-
